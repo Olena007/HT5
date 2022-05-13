c959a27dc1a185121156b1cf8b7cb4bcc077d3cf
lena <llogvinovaaaa@gmail.com>
2022-05-13 23:30:15 +0200

    ht5

---
+++ Online-Store/Online-Store/Console.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Online_Store
+{
+    internal class Console
+    {
+        public void WriteToConsoleArr(string[] arr)
+        {
+            foreach (string str in arr)
+            {
+                Console.Writeline
+            }
+        }
+        //public void WriteToConsole()
+        //{
+        //    Filling fill;
+        //    //fill.GoThrought
+        //}
+    }
+}
---
+++ Online-Store/Online-Store/Furniture.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Online_Store
+{
+    internal class Furniture
+    {
+        string type;
+        string color;
+        string price;
+        string amount;
+
+        public string Name
+        {
+            get { return $"{type} {color} {price} {amount}"; }
+        }
+
+        public Furniture(string type, string color, string price, string amount)
+        {
+            this.type = type;
+            this.color = color;
+            this.price = price;
+            this.amount = amount;
+        }
+    }
+}
